6137e1769fbaa6658a87ab2ff3cf5449f511696e
folomeev1980 <40419806+folomeev1980@users.noreply.github.com>
2020-08-28 23:04:47 +1100

    Update bot.py

--- bot.py
+++ bot.py
@@ -59,7 +59,7 @@ def log(bot, update):
         update.message.reply_text(str(myfile.read()))
 
 
-updater = Updater(TOKEN)
+updater = Updater(TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 #    Commands
